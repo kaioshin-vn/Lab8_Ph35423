8986d2134bedda2302757b96c9e93e69555092e3
Dang Giap <dgiap540@gmail.com>
2024-04-05 15:38:58 +0700

    Add project files.

---
+++ Bai1/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+using Bai1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Bai1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            HttpContext.Session.SetString("name", "Phan Viet The");
+            HttpContext.Session.SetString("email", "thepv@uit.edu.vn.com"); return View();
+        }
+        public IActionResult About()
+        {
+            ViewBag.Name = HttpContext.Session.GetString("name");
+            ViewBag.Email = HttpContext.Session.GetString("email");
+            ViewData["Message"] = "Your about page, please refesh page after one minute"; 
+            ViewData["Title"] = "Demo session login";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
